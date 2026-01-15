f498c776ed7dee399a51d55d7b48673f37ab0a31
Ritesh Kumar <ritkumar5690@gmail.com>
2026-01-15 08:53:40 +0530

    Fix asset imports and router for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import "./assets/css/font-awesome.css";
+import "./index.css";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { HashRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
     <App />
-  </StrictMode>,
-)
+  </HashRouter>
+);
